a99347e322ee05b4d318269233b78b117ba39750
Arun <arunkumard0606@gmail.com>
2024-05-23 23:38:22 +0530

    Initial commit

---
+++ src/main/java/com/example/ibmMq/IbmMqApplication.java
@@ -0,0 +1,14 @@
+package com.example.ibmMq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbmMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbmMqApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
